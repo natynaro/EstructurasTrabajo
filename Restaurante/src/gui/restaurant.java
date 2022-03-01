fe2393ce741d88495be133c1abf98ef80b91e509
juanjo32 <juanjosegiraldo4@gmail.com>
2022-03-01 07:45:26 -0500

    szs

---
+++ Restaurante/src/gui/restaurant.java
@@ -0,0 +1,83 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+
+public class restaurant extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					resta frame = new resta();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public resta() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.LIGHT_GRAY);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Funciones administrativas");
+		btnNewButton.setBackground(Color.RED);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(31, 78, 151, 36);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Platos");
+		btnNewButton_1.setBackground(Color.RED);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(245, 78, 85, 21);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Domiciliarios");
+		btnNewButton_2.setBackground(Color.RED);
+		btnNewButton_2.setBounds(53, 156, 97, 21);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Pedidos");
+		btnNewButton_3.setBackground(Color.RED);
+		btnNewButton_3.setBounds(245, 156, 85, 21);
+		contentPane.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("Clientes");
+		btnNewButton_4.setBackground(Color.RED);
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_4.setBounds(152, 198, 85, 21);
+		contentPane.add(btnNewButton_4);
+	}
+}
