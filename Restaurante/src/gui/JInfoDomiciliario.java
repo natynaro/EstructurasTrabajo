dc90962b4f8ea6f98f65bc0ddbad94c579d6f368
Natalia Naranjo <90767304+natynaro@users.noreply.github.com>
2022-03-04 08:52:10 -0500

    avance

--- Restaurante/src/gui/JInfoDomiciliario.java
+++ Restaurante/src/gui/JInfoDomiciliario.java
@@ -6,10 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JList;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class JInfoDomiciliario extends JFrame {
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+
+public class JInfoDomiciliario extends JFrame implements ActionListener{
 
 	private JPanel contentPane;
+	private JTextField txtNombre;
 
 	/**
 	 * Launch the application.
@@ -35,8 +45,40 @@ public class JInfoDomiciliario extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JList list = new JList();
+		list.setBounds(17, 28, 186, 192);
+		contentPane.add(list);
+		
+		JButton btnvolver = new JButton("volver");
+		btnvolver.setBounds(17, 232, 100, 30);
+		contentPane.add(btnvolver);
+		
+		JLabel lblTitulo = new JLabel("");
+		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitulo.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		lblTitulo.setBounds(227, 50, 200, 41);
+		contentPane.add(lblTitulo);
+		
+		txtNombre = new JTextField();
+		txtNombre.setToolTipText("Ingrese el nombre del domiciliario");
+		txtNombre.setText("nombre:");
+		txtNombre.setBounds(237, 103, 177, 50);
+		contentPane.add(txtNombre);
+		txtNombre.setColumns(10);
+		
+		JButton btnAceptar = new JButton("Aceptar");
+		btnAceptar.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		btnAceptar.setBounds(288, 185, 82, 40);
+		contentPane.add(btnAceptar);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
